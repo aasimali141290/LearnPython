e355d11e7331de9cc4d204acd315b09a17ec6d97
aasim ali <aasimali141290@gmail.com>
2016-11-19 15:17:33 -0500

    line number

--- numberOfLine.py
+++ numberOfLine.py
@@ -1,8 +1,9 @@
 def lineReadFile():
-    a=0
+    a = 0
     file = open('C:\\Users\\aasim ali\\Downloads\\a1.txt', 'r')
     for line in file:
-        if line[:-1]:
-            a+=1
+        a += 1
     print(a)
+
+
 lineReadFile()
--- readfile.py
+++ readfile.py
@@ -3,9 +3,15 @@ def readFile():
     while True:
         line = file.readline()
         if not line:
+            print(line)
             break
         else:
             print(line)
 
-
+def lineReadFile2():
+    a = 0
+    file = open('C:\\Users\\aasim ali\\Downloads\\a1.txt', 'r')
+    for line in file:
+        print(line)
 readFile()
+lineReadFile2()
